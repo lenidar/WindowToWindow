14382bdb5686260335a6d1bf6e2c9e42fb54209b
lenidar <55443074+lenidar@users.noreply.github.com>
2023-04-12 11:37:36 +0800

    Add project files.

---
+++ WindowToWindow/wMan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowToWindow
+{
+    internal static class wMan
+    {
+        public static void displayMain()
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+
+        public static void displayAnother()
+        {
+            AnotherWindow aw = new AnotherWindow();
+            aw.Show();
+        }
+    }
+}
